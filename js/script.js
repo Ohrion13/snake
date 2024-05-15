dadd0556ad2c797e318436a4887d68cb392243f9
Sylvain Schunk <sylvain.katet@hotmail.fr>
2024-05-15 07:24:19 +0200

    rename functions and clean code

--- js/script.js
+++ js/script.js
@@ -6,8 +6,20 @@ const snakeTailImg = document.getElementById("snakeTail");
 
 let containerLst = document.querySelectorAll('.container-lst');
 
+/**
+ * Gets a cell of the grid at the coordinates x and y
+ * @param {number} x - abscissa of the cell
+ * @param {number} y - ordinate of the cell
+ * @returns {element} - the cell element (li) at the wanted coordinates
+ */
+function getCell(x, y) {
+    return document.querySelector(`[data-x="${x}"][data-y="${y}"]`);
+}
 
-function getGridCoordinates() {
+/**
+ * Sets the coordinates of each cell of the grid by adding data-x and data-y attributes
+ */
+function setGridCoordinates() {
     
     containerLst.forEach(function (cell, index) {
         let x = index % 10;
@@ -18,11 +30,8 @@ function getGridCoordinates() {
     });
 }
 
-getGridCoordinates();
+setGridCoordinates();
 
-function getCoordinates(x, y) {
-    return document.querySelector(`[data-x="${x}"][data-y="${y}"]`);
-}
 
 function moveSnake() {
     const snakeHeadImg = document.getElementById("snakeHead");
@@ -33,31 +42,37 @@ function moveSnake() {
         if (e.key === "ArrowDown") {
             i++;
             if (i > 9) i = 0;
-            const headCell = getCoordinates(j, i);
-            //const bodyCell = getCoordinates(j, i-1)
+            const headCell = getCell(j, i);
+            const bodyCell = getCell(j, i-1);
             headCell.appendChild(snakeHeadImg);
-            //bodyCell.appendChild(snakeBodyImg);
+            bodyCell.appendChild(snakeBodyImg);
         }
 
         else if (e.key === "ArrowRight") {
             j++;
             if (j > 9) j = 0;
-            const headCell = getCoordinates(j, i);
+            const headCell = getCell(j, i);
+            const bodyCell = getCell(j-1, i);
             headCell.appendChild(snakeHeadImg);
+            bodyCell.appendChild(snakeBodyImg);
         }
 
         else if (e.key === "ArrowLeft") {
             j--;
             if (j < 0) j = 9;
-            const headCell = getCoordinates(j, i);
+            const headCell = getCell(j, i);
+            const bodyCell = getCell(j+1, i);
             headCell.appendChild(snakeHeadImg);
+            bodyCell.appendChild(snakeBodyImg);
         }
 
         else if (e.key === "ArrowUp") {
             i--;
             if (i < 0) i = 9;
-            const headCell = getCoordinates(j, i);
+            const headCell = getCell(j, i);
+            const bodyCell = getCell(j, i+1);
             headCell.appendChild(snakeHeadImg);
+            bodyCell.appendChild(snakeBodyImg);
         }
     });
 }

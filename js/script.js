0d3699d1d95f66662a303104a2a2f7b98310dc3a
Ohrion <mortharion13@gmail.com>
2024-05-16 11:07:18 +0200

    change menu by clicking on buttons

--- js/script.js
+++ js/script.js
@@ -1,16 +1,11 @@
-//console.log(snakeBodyImg);
-
-const snakeTailImg = document.getElementById("snakeTail");
-//console.log(snakeTailImg);
-
-let containerLst = document.querySelectorAll('.container-lst');
+const containerLst = document.querySelectorAll('.container-lst');
 
 /**
  * Gets a cell of the grid at the coordinates x and y
  * @param {number} x - abscissa of the cell
  * @param {number} y - ordinate of the cell
  * @returns {element} - the cell element (li) at the wanted coordinates
- */
+*/
 function getCell(x, y) {
     return document.querySelector(`[data-x="${x}"][data-y="${y}"]`);
 }
@@ -19,11 +14,11 @@ function getCell(x, y) {
  * Sets the coordinates of each cell of the grid by adding data-x and data-y attributes
 */
 function setGridCoordinates() {
-    
+
     containerLst.forEach(function (cell, index) {
         let x = index % 10;
         let y = Math.floor(index / 10);
-        
+
         cell.setAttribute('data-x', x);
         cell.setAttribute('data-y', y);
     });
@@ -31,51 +26,163 @@ function setGridCoordinates() {
 
 setGridCoordinates();
 
+function getRandomCoordinates() {
+    const randomX = Math.floor(Math.random() * 10);
+    const randomY = Math.floor(Math.random() * 10);
+    return [randomX, randomY];
+}
+
+function setAppleOnGrid() {
+    const randomCoord = getRandomCoordinates();
+    const appleCell = getCell(randomCoord[0], randomCoord[1]);
+    console.log(appleCell);
+}
+
+setAppleOnGrid();
 
 function moveSnake() {
     const snakeHeadImg = document.getElementById("snakeHead");
     const snakeBodyImg = document.getElementById("snakeBody");
+    const snakeTailImg = document.getElementById("snakeTail");
     let i = 0;
     let j = 0;
     let headCell;
     let bodyCell;
-    
+    let tailCell;
+    let headPos = [];
+    let arrowDirection;
+
     window.addEventListener("keydown", function (e) {
 
         if (e.key === "ArrowDown") {
+            if (arrowDirection === "up") return;
+            arrowDirection = "down";
             i++;
             if (i > 9) i = 0;
-            headCell = getCell(j, i);
-            bodyCell = getCell(j, i-1);
+            headPos.unshift([j, i]);
+            snakeHeadImg.style.transform = "rotate(90deg)"
+            snakeBodyImg.style.transform = "rotate(90deg)"
+            snakeTailImg.style.transform = "rotate(90deg)"
         }
 
         else if (e.key === "ArrowRight") {
+            if (arrowDirection === "left") return;
+            arrowDirection = "right";
             j++;
             if (j > 9) j = 0;
-            headCell = getCell(j, i);
-            bodyCell = getCell(j-1, i);
+            headPos.unshift([j, i]);
+            snakeHeadImg.style.transform = "rotate(0deg)"
+            snakeBodyImg.style.transform = "rotate(0deg)"
+            snakeTailImg.style.transform = "rotate(0deg)"
         }
 
         else if (e.key === "ArrowLeft") {
+            if (arrowDirection === "right") return;
+            arrowDirection = "left";
             j--;
             if (j < 0) j = 9;
-            headCell = getCell(j, i);
-            bodyCell = getCell(j+1, i);   
+            headPos.unshift([j, i]);
+            snakeHeadImg.style.transform = "rotate(180deg)"
+            snakeBodyImg.style.transform = "rotate(180deg)"
+            snakeTailImg.style.transform = "rotate(180deg)"
         }
 
         else if (e.key === "ArrowUp") {
+            if (arrowDirection === "down") return;
+            arrowDirection = "up";
             i--;
             if (i < 0) i = 9;
-            headCell = getCell(j, i);
-            bodyCell = getCell(j, i+1);
+            headPos.unshift([j, i]);
+            snakeHeadImg.style.transform = "rotate(-90deg)"
+            snakeBodyImg.style.transform = "rotate(-90deg)"
+            snakeTailImg.style.transform = "rotate(-90deg)"
         }
+
+        headCell = getCell(j, i);
         headCell.appendChild(snakeHeadImg);
+
+        switch (headPos.length) {
+            case 1:
+                bodyCell = getCell(0, 0);
+                tailCell = getCell(1, 0);
+                break;
+            case 2:
+                bodyCell = getCell(0, 1);
+                tailCell = getCell(0, 0);
+                break;
+            default:
+                bodyCell = getCell(headPos[1][0], headPos[1][1]);
+                tailCell = getCell(headPos[2][0], headPos[2][1]);
+                break;
+        }
+
         bodyCell.appendChild(snakeBodyImg);
+        tailCell.appendChild(snakeTailImg);
+
     });
-}
-
-moveSnake()
-
-function bodyAndTailFollowHead() {
 
 }
+
+moveSnake();
+
+function selectMode(clickedButton) {
+
+    const gameMenu = document.getElementById("menuMode");
+    const gameSize = document.getElementById("menuSize");
+    const gameGrid = document.getElementById("menuGrid");
+    const game = document.getElementById("game");
+
+    switch (clickedButton) {
+
+        case 'selectSinglePlayer':
+
+            gameMenu.style.display = "none";
+            // .style.display = "flex";
+            break;
+
+        case 'selectFastMode':
+
+            gameMenu.style.display = "none";
+            gameSize.style.display = "flex";
+            break;
+
+        case 'selectTwoPlayerMode':
+
+            gameMenu.style.display = "none";
+            // .style.display = "flex";
+            break;
+
+        case 'smallGamingScreen':
+
+            gameSize.style.display = "none";
+            gameGrid.style.display = "grid";
+            game.style.padding = "0";
+            document.body.style.overflow = "hidden";
+            break;
+
+        case 'mediumGamingScreen':
+
+            gameSize.style.display = "none";
+            // .style.display = "grid";
+            game.style.padding = "0";
+            document.body.style.overflow = "hidden";
+            break;
+
+        case 'largeGamingScreen':
+
+            gameSize.style.display = "none";
+            // .style.display = "grid";
+            game.style.padding = "0";
+            document.body.style.overflow = "hidden";
+            break;
+    }
+}
+
+document.getElementById('selectFastMode').addEventListener('click', function () {
+    selectMode(this.id);
+});
+
+document.getElementById('smallGamingScreen').addEventListener('click', function () {
+    selectMode(this.id);
+});
+
